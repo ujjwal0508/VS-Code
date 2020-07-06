4cea693030675466dfc7fc86ca17d09e7495e895
ujjwal0508 <ujjwal.jindal3@gmail.com>
2020-07-05 22:18:19 -0700

    explorer view done

---
+++ index.js
@@ -0,0 +1,71 @@
+const $ = require('jquery');
+const fs = require('fs');
+const nodePath = require('path');
+// const monaco = require('monaco-editor');
+
+
+let currPath;
+let directory;
+$(document).ready(function () {
+
+
+    // monaco.editor.create(document.getElementById("text-editor"), {
+    //     value: "function hello() {\n\talert('Hello world!');\n}",
+    //     language: "javascript"
+    // });
+    
+
+    currPath = process.argv[6].split('=')[1];
+    //--app-path=C:\Users\INDIA\Desktop\1. Dev\2. Lectures\Vs-Code-Clone
+    let data = [];
+    data.push({
+        id: currPath,
+        parent: '#',
+        text: getName(currPath)
+    })
+
+    data = data.concat(getCurrentDirectories(currPath));
+
+    $('#content').jstree({
+        "core": {
+            data: data,
+            "check_callback": true
+        }
+        
+    }).on("open_node.jstree", function (e, data) {
+
+        data.node.children.forEach(function (child) {
+            let directories = getCurrentDirectories(child);
+
+            directories.forEach(function (directory) {
+                let temp = $('#content').jstree().create_node(child, directory, "last");
+            })
+        })
+    }).on('changed.jstree', function (e, data) {
+        console.log(data.selected)
+    })
+})
+
+function getCurrentDirectories(path) {
+
+    if (fs.lstatSync(path).isFile()) {
+        return [];
+    }
+
+    let files = fs.readdirSync(path);
+
+    let rv = [];
+    files.forEach(function (file) {
+        rv.push({
+            id: nodePath.join(path, file),
+            parent: path,
+            text: file
+        });
+    })
+
+    return rv;
+}
+
+function getName(path) {
+    return path.replace(/^.*[\\\/]/, '');
+}
